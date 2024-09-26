d7f1b78518ef38e564bc9ee58dc61a81cb70bed6
akinari <takeyaindustries2022@gmail.com>
2024-09-27 00:17:25 +0900

    ホームに戻る機能追加　など

--- manualToolSample/manualToolSample/boardapp/urls.py
+++ manualToolSample/manualToolSample/boardapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from .views import signupfunc,  loginfunc, listfunc, logoutfunc, detailfunc, goodfunc, readfunc, BoardCreate, homefunc
+from .views import signupfunc, loginfunc, listfunc, logoutfunc, detailfunc, goodfunc, readfunc, BoardCreate, homefunc, \
+    ReadListView
 
 urlpatterns = [
     path('', loginfunc, name='login'),
@@ -11,6 +12,7 @@ urlpatterns = [
     path('logout/', logoutfunc, name='logout'),
     path('detail/<int:pk>', detailfunc, name="detail"),
     path('good/<int:pk>', goodfunc, name="good"),
-    path('read/<int:pk>', readfunc, name="read"),
+    path('read/<int:pk>/', readfunc, name="read"),
+    path('read_list/<int:pk>/', ReadListView.as_view(), name='read_list'),  # 新しいクラスベースビュー
     path('create/', BoardCreate.as_view(), name="create"),
 ]
--- manualToolSample/manualToolSample/boardapp/views.py
+++ manualToolSample/manualToolSample/boardapp/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 from django.urls import reverse_lazy
 
 from .models import BoardModel
@@ -37,7 +37,7 @@ def loginfunc(request):
 
 @login_required
 def listfunc(request):
-    object_list = BoardModel.objects.all()
+    object_list = BoardModel.objects.all().order_by('-id')  # 作成された順（新しい順）に並べ替え
     return render(request, 'list.html', {'object_list': object_list})
 
 def logoutfunc(request):
@@ -54,6 +54,7 @@ def goodfunc(request, pk):
     post.save()
     return redirect('list')
 
+# 既存の関数ベースビューも残す（必要であれば削除）
 def readfunc(request, pk):
     post = BoardModel.objects.get(pk=pk)
     post2 = request.user.get_username()
@@ -65,9 +66,37 @@ def readfunc(request, pk):
         post.save()
         return redirect('list')
 
+
+class ReadListView(ListView):
+    model = BoardModel  # 表示したいモデルを指定
+    template_name = 'list.html'  # 使用するテンプレートファイル
+    context_object_name = 'object_list'  # テンプレートで使うコンテキスト変数名
+
+    def get(self, request, *args, **kwargs):
+        # pkを取得して、特定の投稿の既読処理を行う
+        pk = self.kwargs.get('pk')
+        post = BoardModel.objects.get(pk=pk)
+        username = request.user.get_username()
+
+        # 読んだかどうかの確認と処理
+        if username not in post.readtext:
+            post.read += 1
+            post.readtext += f' {username}'
+            post.save()
+
+        # 既存のListViewの処理（投稿リストを表示）
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['message'] = "特定の投稿が既読として登録されました。"  # 必要なら追加の情報をコンテキストに渡す
+        return context
+
 def homefunc(request):
     return render(request, 'home.html')
 
+# クラスベースビュー
+# Djangoにおいてビューの機能をクラスとして定義する方法。所謂、少ない記述でCRUDの機能を作れる汎用クラス
 class BoardCreate(CreateView):
     template_name = 'create.html'
     model = BoardModel
