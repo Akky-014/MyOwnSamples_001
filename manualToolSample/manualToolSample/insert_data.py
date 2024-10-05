fcb74c54e4365a087cde07143b12a753f5c893ee
akinari <takeyaindustries2022@gmail.com>
2024-10-05 23:38:55 +0900

    docker insert 一部修正

--- manualToolSample/manualToolSample/insert_data.py
+++ manualToolSample/manualToolSample/insert_data.py
@@ -15,11 +15,13 @@ events = [
     {'eventName': 'Event1', 'packageName': 'com.example.package1', 'componentName': 'Component1', 'nodeViewId': 'ViewId1', 'nodeAddress': 'Address1', 'images': 'example_image1.jpg', 'detail': 'This is a detail for Event 1'},
     {'eventName': 'Event2', 'packageName': 'com.example.package2', 'componentName': 'Component2', 'nodeViewId': 'ViewId2', 'nodeAddress': 'Address2', 'images': 'example_image2.jpg', 'detail': 'This is a detail for Event 2'},
     {'eventName': 'Event3', 'packageName': 'com.example.package3', 'componentName': 'Component3', 'nodeViewId': 'ViewId3', 'nodeAddress': 'Address3', 'images': 'example_image3.jpg', 'detail': 'This is a detail for Event 3'},
-    {'eventName': 'Event4', 'packageName': 'com.example.package4', 'componentName': 'Component4', 'nodeViewId': 'ViewId4', 'nodeAddress': 'Address4', 'images': 'example_image4.jpg', 'detail': 'This is a detail for Event 3'},
+    {'eventName': 'Event4', 'packageName': 'com.example.package4', 'componentName': 'Component4', 'nodeViewId': 'ViewId4', 'nodeAddress': 'Address4', 'images': 'example_image4.jpg', 'detail': 'This is a detail for Event 4'},
+    # {'eventName': 'Event5', 'packageName': 'com.example.package5', 'componentName': 'Component5', 'nodeViewId': 'ViewId5', 'nodeAddress': 'Address5', 'images': 'example_image5.jpg', 'detail': 'This is a detail for Event 5'},
 ]
 
-# データの挿入
+# データの挿入（すでに存在しない場合のみ）
 for event in events:
-    EventModel.objects.create(**event)
+    if not EventModel.objects.filter(eventName=event['eventName']).exists():
+        EventModel.objects.create(**event)
 
 print("Data inserted successfully.")
